833db6166b155828fe0ec3ebddabd421b39e3599
Lucas Goeyenbier <L.goeyenbier@gmail.com>
2022-05-31 21:20:34 +0200

    second commit to see if you can see all different commits

--- main2.py
+++ main2.py
@@ -3,3 +3,7 @@ print("Hello Git")
 print("test 2")
 
 print("this is to check for the changes into remote repository")
+
+print("making again more comments")
+
+print("Can you see the different commits separately")
